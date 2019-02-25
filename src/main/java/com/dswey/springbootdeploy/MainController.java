4245e17718c940d8d860124a23cd20f16564655c
wangenyong <wangenyong@gmail.com>
2019-02-25 21:30:18 +0800

    Add mysql docker.

--- src/main/java/com/dswey/springbootdeploy/MainController.java
+++ src/main/java/com/dswey/springbootdeploy/MainController.java
@@ -1,14 +1,36 @@
 package com.dswey.springbootdeploy;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping(path = "/")
 public class MainController {
 
-    @RequestMapping("/")
+    @RequestMapping(path = "/")
     public String hello() {
         return "Hello spring deploy";
+    }
+
+    @RequestMapping(path = "/test")
+    public String test() {
+        return  "test";
+    }
+
+    @Autowired
+    UserRepository userRepository;
+
+    @RequestMapping(path = "/add")
+    public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String email) {
+        User n = new User();
+        n.setName(name);
+        n.setEmail(email);
+        userRepository.save(n);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody String getAllUsers() {
+        return "ok";
     }
 }
